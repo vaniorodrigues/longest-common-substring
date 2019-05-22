7819af7255454caf8fe95da3d15627cca1467f0e
Vanio Rodrigues <rodrigues.vanio@gmai.com>
2019-05-22 09:28:28 -0300

    Funcionando mas nao ta passando no teste.

--- lcs.cpp
+++ lcs.cpp
@@ -2,17 +2,8 @@
 #include<string>
 #include<map>
 #include <vector>
+#include <stdio.h>
 
-#include <cstdio>
-#include <algorithm>
-#include <cstring>
-#include <utility>
-#include <queue>
-#include <stack>
-#include <set>
-#include <cmath>
-#include <cassert>
-#include <ctime>
 
 using namespace std;
 
@@ -83,32 +74,17 @@ int Substring(string s, string w) {// S é a string que o automato de suffixo re
         }
         n = a.edges[n][w[i]];
     }
-    //if(!fail) cout << w.size() <<"\n";
-    //cout << w.size() << " is a substring of " << '\n';
     return w.size();
-}
-
-int LongestCS_2(string s,string w){ // Longest Common Substring between two strings.
-    int ans = 0; // Se o tamanho de S for maior, então ele será o SA, e w (assim como suas subtrings) será testado.
-    ans = Substring (s,w); // Primeiro teste é pra ver se o próprio w não é um substring de s.
-    for (int l = w.size()-1; l > 0; l--) { // Comprimento do substring vai de w.size -1, até 1. Sendo que as possibilidades de i vai depender do comrpimento l.;
-        for (int i = 0; i <= w.size() - l; i++) { // i vai de 0 até a diferença entre o tamanho e o comprimento da string
-            if (ans != 0) {
-                goto end;// Na primeira saida que encontrar um tamanho da string diferente de zero, ou seja a substring permanece a outra string, então sai do loop e manda o valor pro main. Ele vai testando do maior valor para o menor, entao nao precisa testar tudo, só precisa testar até achar o primeiro match.
-            }
-            string sub = w.substr(i,l);
-            ans = Substring (s, sub);
-        }
-    }
-    end:
-    cout << "ans=" << ans << "\n";
-    return ans;
 }
 
 int LongestCS(int k,vector<string> stringInput){ // A primeira string vai ser testada nos Automatos de Sufixos gerados pelas outras strings.
     int substringLenght = 0;
     string in0 = stringInput[0]; // Usado só pra facilitar a nomenclatura
     int isSubstri =1;
+    if (k==1){
+        substringLenght = in0.size();
+        goto end;
+    }
     for (int j = 1; j < k; j++) { // Teste se a primeira string é um substring das outras strigs.
         isSubstri = Substring(stringInput[j],stringInput[0]) * isSubstri;
     }
@@ -116,48 +92,33 @@ int LongestCS(int k,vector<string> stringInput){ // A primeira string vai ser te
         substringLenght = in0.size();
         goto end; // Hora de sair da função.
     }
-
-    if (k==1){
-        substringLenght = in0.size();
-    } else {
-        for (int l = in0.size()-1; l > 0; l--) { // Comprimento do substring vai de w.size -1, até 1. Sendo que as possibilidades de i vai depender do comrpimento l.;
-            for (int i = 0; i <= in0.size() - l; i++) { // i vai de 0 até a diferença entre o tamanho e o comprimento da string
-                if (substringLenght != 0) {
-                    goto end;// Na primeira saida que encontrar um tamanho da string diferente de zero, ou seja a substring permanece a outra string, então sai do loop e manda o valor pro main. Ele vai testando do maior valor para o menor, entao nao precisa testar tudo, só precisa testar até achar o primeiro match.
-                }
-                string sub_in0 = in0.substr(i,l);
-                int isSubstri =1;
-                for (int j = 1; j < k; j++) { // Teste se alguma  dada substring de também é uma substring das outras strings. Começa em 1 já que k = 0 é a primeira string, sendo desnecessário este testa-lá.
-                    isSubstri = Substring(stringInput[j],sub_in0) * isSubstri; // Se um dos valores de substring for zero, nao vai pra saida.
-                }
-                if ( isSubstri !=0 ) { //Se isSubstri é diferente de 0, significa que a última substring testado é uma substring de todas as strings de entrada.
-                    substringLenght = sub_in0.size();
-                    goto end; // Hora de sair da função.
-                }
+    for (int l = in0.size()-1; l > 0; l--) { // Comprimento do substring vai de w.size -1, até 1. Sendo que as possibilidades de i vai depender do comrpimento l.;
+        for (int i = 0; i <= in0.size() - l; i++) { // i vai de 0 até a diferença entre o tamanho e o comprimento da string
+            if (substringLenght != 0) {
+                goto end;// Na primeira saida que encontrar um tamanho da string diferente de zero, ou seja a substring permanece a outra string, então sai do loop e manda o valor pro main. Ele vai testando do maior valor para o menor, entao nao precisa testar tudo, só precisa testar até achar o primeiro match.
+            }
+            string sub_in0 = in0.substr(i,l);
+            int isSubstri =1;
+            for (int j = 1; j < k; j++) { // Teste se alguma  dada substring de também é uma substring das outras strings. Começa em 1 já que k = 0 é a primeira string, sendo desnecessário este testa-lá.
+                isSubstri = Substring(stringInput[j],sub_in0) * isSubstri; // Se um dos valores de substring for zero, nao vai pra saida.
+            }
+            if ( isSubstri !=0 ) { //Se isSubstri é diferente de 0, significa que a última substring testado é uma substring de todas as strings de entrada.
+                substringLenght = sub_in0.size();
+                goto end; // Hora de sair da função.
             }
         }
-        return 0; // Nenhuma substring foi encontrada.
     }
-    end:
-    return substringLenght; // Retorna o valor do comprimento da maior substring comum entre as strings de entrada.
-
+    end: return substringLenght; // Retorna o valor do comprimento da maior substring comum entre as strings de entrada.
 }
-
-
 /* */
 int main ()
 {
-    /* Entrada de argumentos
-
-
-    scanf("%d",&T);
-    scanf("%d",&K);*/
-
     int t; // Teste cases
-    int k; // Número de strings em um teste.
+    scanf("%d", &t);
+     // Número de strings em um teste.
     int out;
-    scanf("%d\n", &t);
     for (int i = 0; i < t; i++) {
+        int k;
         scanf("%d", &k);
         char a[k][10001];
         vector<string> stringInput; //  Local onde é armazenado as strings de entrada.
